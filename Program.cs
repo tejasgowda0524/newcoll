97c17078dde621e1a8d87239fdd0d62d2b99f8f8
TEJAS <tejasgowda0524@gmail.com>
2024-02-23 12:20:17 +0530

    added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace newcollab
+{
+
+    public class add
+    {
+        public int addition(int i,int j)
+        {
+            return i + j;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
